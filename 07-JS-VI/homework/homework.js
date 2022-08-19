405ad54b5558e14c257de68d854b8209e50b8e4a
adrialexis <adrian.alexis.vega@gmail.com>
2022-08-19 14:15:19 -0500

    js vI logro

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,21 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  nombre
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb ()
 }
 
 function operacionMatematica(n1, n2, cb) {
-  //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
+  //Vamos a recibir una función que realiza una operación
+  //matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+ cb(n1,n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +26,22 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let  iniciasuma  = 0;
+  let  sumfinal  = numeros.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    iniciasuma
+  );
+  
+  cb (sumfinal)
+ 
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+ let finarray = []
+  
 }
 
 function map(array, cb) {
